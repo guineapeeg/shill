e1ca90bfa81cbc26522ca7737250c81d3b254704
Guineapeeg <marymary@gmail.com>
2023-06-02 18:27:59 -0700

    kill process

--- cshell.c
+++ cshell.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+
+    char *name;
+    char *value;
+
+} EnvVar;
+
+int print();
+
 int main(int argc, char *argv[]){
 
+    
+}
+
+int print(){
+
     printf("cshell$");
+
+    return (0);
+
+}
+
+int exit(){
+
+    kill(getpid(), SIGINT);
 
     return (0);
 }
